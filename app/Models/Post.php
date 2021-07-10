9978aebc1311b0b653b83692404e03cb851bd109
cheetos22 <cheetos22@gmail.com>
2021-07-10 13:21:43 +0200

    add filable columns in post table

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+     /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'title',
+        'body',
+    ];
 }
