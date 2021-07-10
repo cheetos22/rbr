9194b5b75659a6751f88b282210001866bb083d9
cheetos22 <cheetos22@gmail.com>
2021-07-10 22:05:05 +0200

    add relation to Post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Query\Builder;
 
 class Post extends Model
 {
@@ -20,4 +22,15 @@ class Post extends Model
         'title',
         'body',
     ];
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'user_id', 'user_id');
+    }
+    //Try to do scope but not working :((
+    //In artisan Tinker giveme an error, to investigate later
+    public function scopeBestActivityLastWeek(Builder $query) : Builder
+    {
+        return $query->whereDate('created_at', '>', Carbon::now()->addDays(-7));
+    }
 }
