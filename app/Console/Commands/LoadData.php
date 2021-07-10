9c60fae500a8aa6335b85135a33af668a89bac1d
cheetos22 <cheetos22@gmail.com>
2021-07-10 16:11:42 +0200

    add synchornize usersDara to LoadData command

--- app/Console/Commands/LoadData.php
+++ app/Console/Commands/LoadData.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Http;
 use App\Models\Post;
+use App\Models\User;
 
 class LoadData extends Command
 {
@@ -44,9 +45,9 @@ class LoadData extends Command
 
         foreach ($postsResponse as $postData) {
             //dd($postData['id']);
-            $postID = Post::select('post_id')->where('post_id' , $postData['id'])->first();
+            $postId = Post::select('post_id')->where('post_id' , $postData['id'])->first();
             //dd($postID);
-            if ($postID === null) {
+            if ($postId === null) {
                 Post::create([
                     'user_id' => $postData['userId'],
                     'post_id' => $postData['id'],
@@ -57,6 +58,37 @@ class LoadData extends Command
 
             };
         };
+
+        //synchronize usersData
+        $usersResponse = Http::get('https://jsonplaceholder.typicode.com/users')->json();
+        //dd($usersResponse);
+        foreach ($usersResponse as $userData) {
+            //dd($userData);
+            $userId = User::select('user_id')->where('user_id' , $userData['id'])->first();
+            //dd($postID);
+            if ($userId === null) {
+                User::create([
+                    'user_id' => $userData['id'],
+                    'name' => $userData['name'],
+                    'user_name' => $userData['username'],
+                    'email' => $userData['email'],
+                    'company_bs' => $userData['company']['bs'],
+                    'company_catchPhrase' =>  $userData['company']['catchPhrase'],
+                    'company_name' =>  $userData['company']['name'],
+                    'website' =>  $userData['website'],
+                    'phone' =>  $userData['phone'],
+                    'adress_geo_lng' =>  $userData['address']['geo']['lng'],
+                    'adress_geo_lat' =>  $userData['address']['geo']['lat'],
+                    'addres_zipcode' =>  $userData['address']['zipcode'],
+                    'adress_city' =>  $userData['address']['city'],
+                    'adress_suite' =>  $userData['address']['suite'],
+                    'adress_street' =>  $userData['address']['street'],
+                ]
+            );
+
+            };
+        };
+
         dump('Succes');
     }
 }
